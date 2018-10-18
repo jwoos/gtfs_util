04d2afaa0d770fd0ba2bca595cafc8d776e6a807
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-10-18 18:26:55 +0000

    fix util case changes

--- gtfs_parser/util.py
+++ gtfs_parser/util.py
@@ -1,9 +1,4 @@
 def camel_to_snake(name):
-    split_name = name.split('_')
-    return ''.join([x.capitalize() for x in split_name])
-
-
-def snake_to_camel(name):
     indices = [0]
 
     for i in range(1, len(name)):
@@ -11,9 +6,19 @@ def snake_to_camel(name):
             indices.append(i)
 
     split_name = []
-    for i in range(1, len(name)):
+    for i in range(1, len(indices)):
         start = indices[i - 1]
         end = indices[i]
-        split_name.append(name[start:end])
+        split_name.append(name[start:end].lower())
 
-    return ''.join(split_name)
+    split_name.append(name[indices[-1]:].lower())
+
+    return '_'.join(split_name)
+
+
+def snake_to_camel(name):
+    split_name = name.split('_')
+    return ''.join([
+        split_name[i].capitalize() if i else split_name[i]
+        for i in range(len(split_name))
+    ])
