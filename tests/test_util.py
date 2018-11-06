32c7a099ee4ecfb09223ec290f0be0cd4e06c92f
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-11-06 19:17:48 +0000

    Added testing library and a simple test

---
+++ tests/test_util.py
@@ -0,0 +1,12 @@
+import unittest
+
+from gtfs_parser import util
+
+
+class TestCamelToSnake(unittest.TestCase):
+    def test_no_upper(self):
+        x = 'ateststring'
+        self.assertEqual(
+            x,
+            util.camel_to_snake(x),
+        )
