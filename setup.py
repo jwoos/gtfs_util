3a377815b13efbcb38f5e82e9d465972f466cc1e
Jun Woo Shin <shin.junwoo97@gmail.com>
2018-11-06 20:26:19 +0000

    remove pipenv dep and state requirements directly

--- setup.py
+++ setup.py
@@ -1,9 +1,4 @@
 import setuptools
-from pipenv.project import Project
-from pipenv.utils import convert_deps_to_pip
-
-
-pfile = Project(chdir=False).parsed_pipfile
 
 
 setuptools.setup(
@@ -14,6 +9,17 @@ setuptools.setup(
     author='Jun Woo Shin',
     license='MIT',
     packages=setuptools.find_packages(),
-    install_requires=convert_deps_to_pip(pfile['packages'], r=False),
-    tests_requires=convert_deps_to_pip(pfile['dev-packages'], r=False),
+    install_requires=[
+        'uvloop',
+        'aiohttp',
+        'protobuf',
+        'requests',
+        'SQLAlchemy',
+        'gtfs-realtime-bindings',
+        'aiofiles',
+    ],
+    tests_requires=[
+        'pytest',
+        'ipython',
+    ],
 )
